df3a23322d75c5160f685227e63ada0968990e7a
Yasen Kostov <qskatak04@gmail.com>
2021-02-28 17:09:46 +0200

    Add :create_room function

--- lib/friendczar/talk.ex
+++ lib/friendczar/talk.ex
@@ -5,4 +5,10 @@ defmodule Friendczar.Talk do
   def list_rooms do
     Repo.all(Room)
   end
+
+  def create_room(attrs \\ %{}) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
 end
--- lib/friendczar_web/controllers/room_controller.ex
+++ lib/friendczar_web/controllers/room_controller.ex
@@ -2,7 +2,8 @@ defmodule FriendczarWeb.RoomController do
   use FriendczarWeb, :controller
 
   alias Friendczar.Talk.Room
-
+  alias Friendczar.Talk
+  alias Friendczar.Repo
 
   def index(conn, _params) do
     render(conn, "index.html")
@@ -12,4 +13,17 @@ defmodule FriendczarWeb.RoomController do
     changeset = Room.changeset(%Room{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
+
+  def create(conn, %{"room" => room_params}) do
+    case Talk.create_room(room_params) do
+      {:ok, room} ->
+        conn
+        |> put_flash(:info, "Room Created!")
+        |> redirect(to: Routes.room_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
 end
