c2ae8dd7db4d6bd71927aeee68afb560f39f46e8
Yasen Kostov <qskatak04@gmail.com>
2021-03-01 11:11:59 +0200

    Enroll if user_sign_in?

--- lib/friendczar/accounts.ex
+++ lib/friendczar/accounts.ex
@@ -13,4 +13,13 @@ defmodule Friendczar.Accounts do
     end
   end
 
+  def user_signed_in?(conn), do: !!current_user(conn)
+
+  def current_user(conn) do
+    user_id = Plug.Conn.get_session(conn, :current_user_id)
+    if user_id, do: Repo.get(User, user_id)
+    end
+
+
+
 end
