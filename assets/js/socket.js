2c2057f351f82b1bae56a7b927878d1ff2391d88
Yasen Kostov <qskatak04@gmail.com>
2021-03-02 12:07:14 +0200

    Test

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -6,26 +6,56 @@
 //
 // Pass the token on params as below. Or remove it
 // from the params if you are not using authentication.
-import {Socket} from "phoenix"
+import {
+  Socket,
+  Presence
+} from "phoenix"
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket("/socket", {
+  params: {
+    token: window.userToken
+  }
+})
 
 let roomId = window.roomId
+let presences = {}
+
+
 socket.connect()
 
 if (roomId) {
-  // Now that you are connected, you can join channels with a topic:
+  const timeout = 3000
+  var typingTimer
+  let userTyping = false
+
+
   let channel = socket.channel(`room:${roomId}`, {})
-
-
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive("ok", resp => {
+      console.log("Joined successfully", resp)
+      console.log(resp)
+      resp.messages.reverse().map(msg => displayMessage(msg))
+    })
 
+    .receive("error", resp => {
+      console.log("Unable to join", resp)
+    })
 
   channel.on(`room:${roomId}:new_message`, (message) => {
     console.log(message)
     displayMessage(message)
+  })
+
+  channel.on("presence_state", state => {
+    presences = Presence.syncState(presences, state)
+    console.log(presences)
+    displayUsers(presences)
+  })
+
+  channel.on("presence_diff", diff => {
+    presences = Presence.syncDiff(presences, diff)
+    console.log(presences)
+    displayUsers(presences)
   })
 
   document.querySelector('#message-form').addEventListener('submit', (e) => {
@@ -39,14 +69,64 @@ if (roomId) {
     input.value = ''
   })
 
+  document.querySelector('#message-body').addEventListener('keydown', () => {
+    userStartsTyping()
+    clearTimeout(typingTimer)
+  })
+  document.querySelector('#message-body').addEventListener('keyup', () => {
+    clearTimeout(typingTimer)
+    typingTimer = setTimeout(userStopTyping, timeout)
+  })
+
+  const userStartsTyping = () => {
+    if (userTyping) {
+      return
+    }
+
+    userTyping = true
+    channel.push('user:typing', {
+      typing: true
+    })
+  }
+
+  const userStopTyping = () => {
+    clearTimeout(typingTimer)
+    userTyping = false
+
+    channel.push('user:typing', {
+      typing: false
+    })
+  }
+
   const displayMessage = (msg) => {
+    console.log('display message')
     let template = `
-      <li class="list-group-item"> ${msg.body} </li>
-    `
+    <li class="list-group-item"><strong>${msg.user.username}</strong>: ${msg.body}</li>
+  `
 
     document.querySelector('#display').innerHTML += template
   }
+
+  const displayUsers = (presences) => {
+    let usersOnline = Presence.list(presences, (_id, {
+      metas: [
+        user, ...rest
+      ]
+    }) => {
+      var typingTemplate = ''
+      if (user.typing) {
+        typingTemplate = ' <i>(Typing...)</i>'
+      }
+      return `
+        <div id="user-${user.user_id}"><strong class="text-secondary">${user.username}</strong> ${typingTemplate}</div>
+      `
+    }).join("")
+
+    document.querySelector('#users-online').innerHTML = usersOnline
+  }
+
 }
+
 
 
 
--- lib/friendczar_web/channels/room_channel.ex
+++ lib/friendczar_web/channels/room_channel.ex
@@ -1,14 +1,62 @@
-defmodule FriendczarWeb.RoomChannel do
+defmodule FriendCzarWeb.RoomChannel do
   use FriendczarWeb, :channel
 
-  def join("room <> room_id", _params, socket) do
-    {:ok, %{channel: "room:#{room_id}"}, assign(socket, :room_id, room_id)}
+  alias Friendczar.Repo
+  alias FriendCzar.Accounts.User
+  alias FriendCzarWeb.Presence
+  alias FriendCzar.Talk.Message
+  alias FriendCzar.Talk
+
+  def join("room:" <> room_id, _params, socket) do
+    send(self(), :after_join)
+    {:ok, %{messages: Talk.list_messages(room_id)}, assign(socket, :room_id, room_id)}
   end
 
   def handle_in("message:add", %{"message" => body}, socket) do
-    room_id = socket.assigns[:room_id]
-    broadcast!(socket, "room:#{room_id}:new_message", %{body: body})
+    room = Talk.get_room!(socket.assigns[:room_id])
+    user = get_user(socket)
+
+    case Talk.create_message(user, room, %{body: body}) do
+      {:ok, message} ->
+        message = Repo.preload(message, :user)
+        message_template = %{body: message.body, user: %{username: message.user.username}}
+        broadcast!(socket, "room:#{message.room_id}:new_message", message_template)
+        {:reply, :ok, socket}
+
+      {:error, _} ->
+        {:reply, :error, socket}
+    end
+  end
+
+  def handle_in("user:typing", %{"typing" => typing}, socket) do
+    user = get_user(socket)
+
+    {:ok, _} =
+      Presence.update(socket, "user:#{user.id}", %{
+        typing: typing,
+        user_id: user.id,
+        username: user.username
+      })
+
     {:reply, :ok, socket}
   end
 
+  def handle_info(:after_join, socket) do
+    push(socket, "presence_state", Presence.list(socket))
+
+    user = get_user(socket)
+
+    {:ok, _} =
+      Presence.track(socket, "user:#{user.id}", %{
+        typing: false,
+        user_id: user.id,
+        username: user.username
+      })
+
+    {:noreply, socket}
+  end
+
+  def get_user(socket) do
+    Repo.get(User, socket.assigns[:current_user_id])
+  end
 end
